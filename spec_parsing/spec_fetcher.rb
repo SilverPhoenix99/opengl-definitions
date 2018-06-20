8086c1b5cd6b28fedf7828b675a7961c8674156b
Silver Phoenix <silver.phoenix99@gmail.com>
2018-06-20 23:29:46 +0100

    Replaced curl with open-uri.

--- spec_parsing/spec_fetcher.rb
+++ spec_parsing/spec_fetcher.rb
@@ -1,7 +1,11 @@
+require 'open-uri'
+
 %w'gl glx wgl'.each do |spec|
   puts "reading #{spec}"
-  
-  body = `curl https://cvs.khronos.org/svn/repos/ogl/trunk/doc/registry/public/api/#{ spec }.xml`
-  filename = File.expand_path("../../#{spec}.xml", __dir__)
-  File.write(filename, body)
+
+  filename = File.expand_path("../../#{ spec }.xml", __dir__)
+
+  open("https://github.com/KhronosGroup/OpenGL-Registry/blob/master/xml/#{ spec }.xml?raw=true") do |f|
+    File.write(filename, f.read)
+  end
 end
